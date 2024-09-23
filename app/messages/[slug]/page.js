9534951c2a471c9861b6642472422bbdf55f04bc
SAROJ <080bct075.saroj@pcampus.edu.np>
2024-09-24 04:00:45 +0545

    Day-6: Improved Specific User Message Page

--- app/messages/[slug]/page.js
+++ app/messages/[slug]/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { usersArray } from '@/serverActions/handleUsers'
 
 const messagePage = ({ params }) => {
@@ -18,11 +18,21 @@ const messagePage = ({ params }) => {
     };
     fetchUsersName();
   }, []);
-
+  
   useEffect(() => {
     document.title = `Message ${user.name}`
   }, [user.name])
-
+  
+  const ref = useRef(null)
+  const [inputMessage, setInputMessage] = useState('')
+  const handleMessageSent = (e) => {
+    ref.current.innerText += ``
+    const myMessageDiv = <div className=' flex flex-col border my-5'>
+      <span>Me: </span>
+      <p className=' rounded-xl px-5 text-lg bg-sky-600'>this is a very long message and i don't know where its going to appear on the page but what i know is its not gonna look good</p>
+    </div>
+    ref.current.appendChild(myMessageDiv);
+  }
 
   return (
     <div className='relative h-full w-full'>
@@ -30,12 +40,21 @@ const messagePage = ({ params }) => {
         {user.name}
       </div>
 
-      <div className="messageSection mx-20 border border-gray-500">
-        this is a very long message and i don't know where its going to appear on the page but what i know is its not gonna look good
+      <div ref={ref} className="messageSection mx-20 ">
+        <div className=' flex flex-col border my-5'>
+          <span>Me: </span>
+          <p className=' rounded-xl px-5 text-lg bg-sky-600'>this is a very long message and i don't know where its going to appear on the page but what i know is its not gonna look good</p>
+        </div>
+
+
       </div>
 
       <div className='messageBox flex items-center justify-center absolute bottom-0 w-full z-50'>
-        <input id='message' name='message' type="text" placeholder='Type Your Message Here' className='w-1/2 py-4 px-8 text-lg text-center box-border rounded-full border border-sky-600 text-white bg-black' />
+        <div className='w-1/2 relative'>
+          <input id='message' name='message' type="text" value={inputMessage} placeholder='Type Your Message Here' onKeyDown={(e) => { if (e.key == "Enter") handleMessageSent() }} onChange={(e) => setInputMessage(e.target.value)} className=' py-4 px-8 w-full text-lg box-border rounded-full border border-sky-600 text-white bg-black' />
+
+          <button onClick={handleMessageSent} className='absolute top-1/2 right-4 transform -translate-y-1/2 bg-blue-600 text-white py-2 px-4 rounded-full'>Send</button>
+        </div>
       </div>
     </div>
   )
